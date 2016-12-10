e27ff3642058bd606762c34a75da2c8248cd12f0
Sskingdon <B00505028@nut.edu.tw>
2016-12-10 13:32:07 +0800

    高公局測試

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -20,7 +20,7 @@ public class BasicExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "http://.../";
+		String uri = "gz:http://tisvcloud.freeway.gov.tw/roadlevel_value.xml.gz";
 
 		System.out.println(
 				CrawlerPack.start()
@@ -30,12 +30,12 @@ public class BasicExample {
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
 				
 				// 選擇資料格式 (三選一)
-				.getFromJson(uri)
+				.getFromXml(uri)
 			    //.getFromHtml(uri)
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
-			    .select(".css .selector ")
+
 			    
 		);
 	}
