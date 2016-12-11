ab4184f858c42165383c357d5bd8e8d56d939743
Sskingdon <B00505028@nut.edu.tw>
2016-12-11 11:33:21 +0800

    八卦版費雯maincontext

---
+++ src/crawler/example/PTTEXAM.java
@@ -0,0 +1,34 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.nodes.Element;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PTTEXAM {
+	// commit test
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481374603.A.18B.html";
+
+        Element jsoup = CrawlerPack.start().addCookie("over18","1").getFromHtml(uri);
+        //System.out.println(jsoup);
+        for (Element ele:jsoup.select("#main-content *")) {
+            ele.remove();
+            //System.out.println(ele);
+        }
+        System.out.println(jsoup.select("#main-content").text());
+    }
+}
